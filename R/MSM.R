48a65d5a33984c4d200c1396c21603db91912d34
klaus <kgf@omniversum.de>
2021-04-19 22:01:27 +0200

    Renaming

--- R/MSM.R
+++ R/MSM.R
@@ -7,32 +7,32 @@ source("R/feedback.R")
 source("R/utils.R")
 
 
-#' BDS
+#' MSM
 #'
-#' This function defines a BDS  module for incorporation into a
+#' This function defines a MSM  module for incorporation into a
 #' psychTestR timeline.
-#' Use this function if you want to include the BDS in a
+#' Use this function if you want to include the MSM in a
 #' battery of other tests, or if you want to add custom psychTestR
 #' pages to your test timeline.
-#' For demoing the BDS, consider using \code{\link{BDS_demo}()}.
-#' For a standalone implementation of the BDS,
-#' consider using \code{\link{BDS_standalone}()}.
+#' For demoing the MSM, consider using \code{\link{MSM_demo}()}.
+#' For a standalone implementation of the MSM,
+#' consider using \code{\link{MSM_standalone}()}.
 #' @param num_items (Integer scalar) Number of items in the test.
 #' @param with_training (Logical scalar) Whether to include the training phase.
 #' @param with_welcome (Logical scalar) Whether to show a welcome page.
 #' @param with_finish (Logical scalar) Whether to show a finished page.
-#' @param label (Character scalar) Label to give the BDS results in the output file.
+#' @param label (Character scalar) Label to give the MSM results in the output file.
 #' @param feedback (Function) Defines the feedback to give the participant
 #' at the end of the test.
 #' @param dict The psychTestR dictionary used for internationalisation.
 #' @export
-BDS <- function(num_items = 18L,
+MSM <- function(num_items = 18L,
                 with_welcome = TRUE,
                 with_training = TRUE,
                 with_finish = TRUE,
-                label = "BDS",
-                feedback = BDS_feedback_with_score(),
-                dict = BDS::BDS_dict) {
+                label = "MSM",
+                feedback = MSM_feedback_with_score(),
+                dict = MSM::MSM_dict) {
   stopifnot(purrr::is_scalar_character(label),
             purrr::is_scalar_integer(num_items) || purrr::is_scalar_double(num_items),
             psychTestR::is.timeline(feedback) ||
@@ -42,7 +42,7 @@ BDS <- function(num_items = 18L,
 
   psychTestR::join(
     psychTestR::begin_module(label),
-    if (with_welcome) BDS_welcome_page(),
+    if (with_welcome) MSM_welcome_page(),
     if (with_training) psychTestR::new_timeline(practice(), dict = dict),
     psychTestR::new_timeline(
       main_test(label = label, num_items_in_test = num_items),
@@ -50,6 +50,6 @@ BDS <- function(num_items = 18L,
     scoring(),
     psychTestR::elt_save_results_to_disk(complete = TRUE),
     feedback,
-    if(with_finish) BDS_finished_page(),
+    if(with_finish) MSM_finished_page(),
     psychTestR::end_module())
 }
