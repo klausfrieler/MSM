71a837619163abf1663f0982fa52d582495c1f79
klaus <kgf@omniversum.de>
2021-05-20 17:05:12 +0200

    Change instructions, practice now for PART2 as well, without practic item.

--- R/practice.R
+++ R/practice.R
@@ -1,7 +1,9 @@
-practice <- function(audio_dir = "https://s3-eu-west-1.amazonaws.com/media.dots.org/stimuli/MSM", type)  {
+practice <- function(audio_dir = "https://s3-eu-west-1.amazonaws.com/media.dots.org/stimuli/MSM",
+                     type,
+                     num_items_test = 10L)  {
   #browser()
   messagef("Added practice pages for type '%s'", type)
-  stim_desc <-  psychTestR::i18n(sprintf("%s_STIMULUS_DESCRIPTION", type))
+  stim_desc <-  psychTestR::i18n(sprintf("%s_STIMULUS_DESCRIPTION", type), sub = list(num_items = num_items_test +1 ))
 
   ret <- psychTestR::one_button_page(body = shiny::div(
     psychTestR::i18n("INSTRUCTIONS", sub = list(stimulus_description = stim_desc)),
@@ -10,21 +12,23 @@ practice <- function(audio_dir = "https://s3-eu-west-1.amazonaws.com/media.dots.
 
   #sample_stimuli <- c("100test_changerule.wav", "101test_proximityrule.wav", "102test_control.wav")
   sample_stimuli <- c("part1_test.wav")
+  if(type == "PART1"){
+    for(i in 1:length(sample_stimuli)){
+      #browser()
+      stimulus <- sample_stimuli[i]
+      header <- get_header(i, length(sample_stimuli), training = T)
 
-  for(i in 1:length(sample_stimuli)){
-    #browser()
-    stimulus <- sample_stimuli[i]
-    header <- get_header(i, length(sample_stimuli), training = T)
+      label <- paste0("s", i)
+      item <- MSM_page(label = label,
+                       stimulus = stimulus,
+                       header = header,
+                       audio_dir = audio_dir,
+                       save_answer = FALSE)
+      ret <- c(ret, item)
+    }
+    ret <- c(ret, psychTestR::one_button_page( body = psychTestR::i18n("CONTINUE_MAIN_TEST"),
+                                               button_text = psychTestR::i18n("CONTINUE")))
 
-    label <- paste0("s", i)
-    item <- MSM_page(label = label,
-                     stimulus = stimulus,
-                     header = header,
-                     audio_dir = audio_dir,
-                     save_answer = FALSE)
-    ret <- c(ret, item)
   }
-  ret <- c(ret, psychTestR::one_button_page( body = psychTestR::i18n("CONTINUE_MAIN_TEST"),
-                                             button_text = psychTestR::i18n("CONTINUE")))
   ret
 }
